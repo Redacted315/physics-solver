2f2902a7d174dbf1a54d0e480cb192c3a3fc3d1c
Redacted315 <matoli315@gmail.com>
2023-03-08 20:28:41 -0700

    first functional version

--- main.py
+++ main.py
@@ -1,48 +1,73 @@
 import math
 
+"""
+To Do:
+    - expand self.requirementsDict with info from docstring at end of file
+    - start developing gui, maybe with pysimplegui so its not as yucky lookin'
+"""
+
+
 class physics:
     def __init__(self):
         self.variableDict = {"velocity":None ,"change_velocity":None,"initial_velocity":None,"final_velocity":None,"acceleration":None,"change_acceleration":None,
                              "time":None,"change_time":None,"distance":None,"change_distance":None,"momentum":None,"change_momentum":None,
-                             "mass":None,"potential_energy":None,"kinetic_energy":None,"height":None,"net_force":None}
+                             "mass":None,"potential_energy":None,"kinetic_energy":None,"height":None,"net_force":None,"applied_force":None,"impulse":None}
+        self.requirementDict = {"mass":['momentum','velocity'],"momentum":['mass','velocity'],"velocity":['change_distance','change_time'],"acceleration":['change_velocity','change_time']}
+        self.possible = []
+    
     def given(self, **known):
         self.known = known
-        # known is dict
-        # if arguments passsed to func given 'v=14, d=37.9'
-        # known.keys() > ['v', 'd']
-        # known.values() > [14, 37.9]
         for variable in self.variableDict:
             if variable in list(known.keys()):
                 self.variableDict[variable] = known[variable]
-        print(self.variableDict)
+        print("given",self.known)
         self.variableDict = self.variableDict
-        for variable in self.variableDict:
-            if self.variableDict[variable] is not None:
-                print(variable + " is known")
+    
     def available(self):
-        if self.variableDict["velo"] is not None and self.variableDict["mass"] is not None:
-            return "you can calculate momentum > m * v = p"
-lightningMcqueen = physics()
-lightningMcqueen.given(velo=1.2, mass=0.5, dist=134)
-print(lightningMcqueen.available())
+        for key in self.requirementDict:
+            if self.variableDict[key] is None:
+                print("testing for",key)
+                print(key,"has",len(self.requirementDict[key]),"requirements")
+                reqs = len(self.requirementDict[key])
+                reqs_met = 0
+                for i in self.requirementDict[key]:
+                    if self.variableDict[i] is None:
+                        print("missing requirement",i)
+                    else:
+                        # print("requirement",i,"satisfied")
+                        reqs_met += 1
+                        if reqs_met == reqs:
+                            print("all requirements met")
+                            self.possible.append(key)
+        print(self.possible)
 
+def main():
+    lightningMcqueen = physics()
+    lightningMcqueen.given(velocity=5,momentum=35)
+    lightningMcqueen.available()
+
+if __name__ == "__main__":
+    main()
 
 """
 requirements:
-
-m(p,v)
+-m(p,v)
 m(Ep,h)#equations line 25
-p(m,v)
-v(∆d,∆t)
+-p(m,v)
+-v(∆d,∆t)
 a(∆v,∆t)
 t(∆d,v)
 t(∆v,a)
-Ek(m,v)
+Ek(m,v)Ek = 0.5 * m * v^2
 Ep(m,h)
 impulse(m,∆v)
 impulse(F,∆t)
 d(vi,t,a)
 d(vf,t,a)
 d(vi,vf,t)
-
+F(m,a)
+F(∆t,∆v,m)#equations line 21
+m(F,∆t,∆v)#equations line 21
+∆v(F,∆t,m)#equations line 21
+∆t(m,∆v,F)#equations line 21
 """
