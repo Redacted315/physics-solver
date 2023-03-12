21ccf29532396fbd528958638a94002d57722fd7
Redacted315 <matoli315@gmail.com>
2023-03-11 23:10:13 -0700

    pythonification

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
-import math
 
 """
 To Do:
     - expand self.requirementsDict with info from docstring at end of file
-    - start developing gui, maybe with pysimplegui so its not as yucky lookin'
 """
 
 
-class physics:
+class Physics:
     def __init__(self):
-        self.variableDict = {"velocity":None ,"change_velocity":None,"initial_velocity":None,"final_velocity":None,"acceleration":None,"change_acceleration":None,
-                             "time":None,"change_time":None,"distance":None,"change_distance":None,"momentum":None,"change_momentum":None,
-                             "mass":None,"potential_energy":None,"kinetic_energy":None,"height":None,"net_force":None,"applied_force":None,"impulse":None}
-        self.requirementDict = {"mass":['momentum','velocity'],"momentum":['mass','velocity'],"velocity":['change_distance','change_time'],"acceleration":['change_velocity','change_time']}
+        self.known = None
+        self.variableDict = {"velocity": None, "change_velocity": None, "initial_velocity": None,
+                             "final_velocity": None, "acceleration": None, "change_acceleration": None,
+                             "time": None, "change_time": None, "distance": None, "change_distance": None,
+                             "momentum": None, "change_momentum": None,
+                             "mass": None, "potential_energy": None, "kinetic_energy": None, "height": None,
+                             "net_force": None, "applied_force": None, "impulse": None}
+        self.requirementDict = {"mass": ['momentum', 'velocity'], "momentum": ['mass', 'velocity'],
+                                "velocity": ['change_distance', 'change_time'],
+                                "acceleration": ['change_velocity', 'change_time']}
         self.possible = []
-    
+
     def given(self, **known):
         self.known = known
         for variable in self.variableDict:
             if variable in list(known.keys()):
                 self.variableDict[variable] = known[variable]
         self.variableDict = self.variableDict
-    
+
     def available(self):
         for key in self.requirementDict:
             if self.variableDict[key] is None:
@@ -35,6 +39,7 @@ class physics:
                         if reqs_met == reqs:
                             self.possible.append(key)
         return self.possible
+
 
 """
 requirements:
